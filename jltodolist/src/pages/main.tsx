1bdca0ac6ad120cbf4070608b41066a5cde2bd70
jordanlat <jordanlat@hotmail.fr>
2021-01-12 15:31:18 +0100

    title

---
+++ jltodolist/src/pages/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonButton } from '@ionic/react';
+
+const Main: React.FC = () => (
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>JOJO TODOLIST</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent>
+
+    </IonContent>
+  </IonPage>
+);
+
+export default Main;
