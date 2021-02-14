7eb42c61abe312ae871ea2e96b4a947a8a6441b2
jordanlat <40663699+jordanlat@users.noreply.github.com>
2021-02-14 22:28:24 +0100

    nettoyage code + suppression fonctionnel  + essaie filtre

--- jltodolist/src/components/task.tsx
+++ jltodolist/src/components/task.tsx
@@ -1,77 +1,48 @@
 import React, { useState } from 'react';
-import { IonItem, IonCheckbox, IonRow, IonCol, IonGrid, IonButton, IonIcon, IonInput, IonModal, IonLabel } from '@ionic/react';
-import { create } from 'ionicons/icons';
+import { IonCheckbox, IonCol, IonRow } from '@ionic/react';
+
 
 const MyTask = (props) => {
 
     const task = props.data;
     const thisIndex = props.index;
-    const [isDisable, setIsDisable] = useState(false);
-    const [checked, setChecked] = useState(false);
-
-    console.log("MA task: ", task);
-    console.log("KEY: ", props.index);
-
+    const [checked, setChecked] = useState(task.isDone);
 
 
     function onCheck(event) {
-        console.log(event);
-        console.log(event.detail.checked);
-
         const myData = JSON.parse(localStorage.getItem("todolist"));
 
         myData[thisIndex] = {
             "label": task.label,
             "isDone": event.detail.checked
         }
-        console.log("Update DATA: ", myData);
+
         localStorage.setItem("todolist", JSON.stringify(myData));
     }
 
-    function rayer(){
-        if(task.isDone) {
+    function rayer() {
+        if (task.isDone) {
             return "rayer"
         }
     }
 
 
-    console.log("IS DISABLE: ", isDisable);
     return (
 
-        <IonItem >
-            <IonGrid>
-                <IonRow>
-                    <IonCol size="2">
-                        <IonCheckbox checked={task.isDone} onIonChange={(e) => {
-                            setChecked(task.isDone = e.detail.checked);
-                            onCheck(e);
-                        }} />
-                    </IonCol>
-                    <IonCol>
-                        <h3 className={rayer()}>{task.label}</h3> 
-                    </IonCol>
-                    <IonCol size="2">
-                        <IonButton
-                            onClick={
-                                () => {
-                                    if (isDisable === false) {
-                                        setIsDisable(true);
-                                    } else {
-                                        setIsDisable(false);
-                                    }
-                                }
-                            }
-                        >
-                            <IonIcon icon={create} />
-                        </IonButton>
-                    </IonCol>
-                </IonRow>
-            </IonGrid>
-            <IonModal isOpen={isDisable} cssClass='my-custom-class'>
-                <p color="dark">This is modal content</p>
-                <IonButton onClick={() => setIsDisable(false)}>Close Modal</IonButton>
-            </IonModal>
-        </IonItem>
+
+        <IonRow>
+            <IonCol size="2">
+                <IonCheckbox checked={checked} onIonChange={(e) => {
+                    task.isDone = e.detail.checked
+                    setChecked(task.isDone);
+                    onCheck(e);
+                }} />
+            </IonCol>
+            <IonCol>
+                <h3 className={rayer()}>{task.label}</h3>
+            </IonCol>
+        </IonRow>
+
     )
 };
 
--- jltodolist/src/pages/main.tsx
+++ jltodolist/src/pages/main.tsx
@@ -1,22 +1,19 @@
 import React, { useState } from 'react';
-import { IonContent, IonInput, IonHeader, IonPage, IonTitle, IonToolbar, IonFooter, IonButton, IonList, IonGrid, IonRow } from '@ionic/react';
+import { IonContent, IonInput, IonHeader, IonPage, IonTitle, IonToolbar, IonFooter, IonButton, IonList, IonGrid, IonRow, IonLabel, IonSegment, IonSegmentButton, IonCol, IonIcon, IonItem } from '@ionic/react';
 
 /** Mes comps */
 import MyTask from '../components/task';
-import Tab from '../components/tab';
-
+import { trash } from 'ionicons/icons';
 
 const Main: React.FC = () => {
-
   const [text, setText] = useState<any>();
   let [tableau, setTableau] = useState<any>([]);
-
-  // Reset 
+  console.log("start");
+  // Reset data
   // localStorage.setItem("todolist", JSON.stringify([]));
   // return
   const myData = JSON.parse(localStorage.getItem("todolist"));
   tableau = myData;
-  // const oldTableau = [... tableau, myData];
 
   function getInput() {
     return text;
@@ -24,36 +21,83 @@ const Main: React.FC = () => {
 
   function addElement() {
     const value = getInput();
-    // console.log(value);
+
     if (value === undefined || value === null || value === " " || value === "") {
       return
     }
-      const myObject = {
-        "isDone": false,
-        "label": value
-      };
-      tableau = [...tableau, myObject];
-      setTableau(tableau);
-      setText("");
-
-      localStorage.setItem("todolist", JSON.stringify(tableau));
-    
+    const myObject = {
+      "label": value,
+      "isDone": false
+    };
+    tableau = [...tableau, myObject];
+    setTableau(tableau);
+    setText("");
+    localStorage.setItem("todolist", JSON.stringify(tableau));
   }
+
+
+  // ne marche pas tout a fait comme je veux
+  function filter(e) {
+    const tab = e.detail.value;
+    let filtreTableau = [];
+    if (tab === "todo") {
+      tableau.filter(task => task.isDone === false).map(filterTask => (
+        filtreTableau = [...filtreTableau, filterTask]
+      ));
+      setTableau(filtreTableau);
+    } else {
+      const storeData = JSON.parse(localStorage.getItem("todolist"));
+      setTableau(storeData);
+    }
+  }
+
+
+  function removeTask(index) {
+    tableau.splice(index, 1);
+    localStorage.setItem("todolist", JSON.stringify(tableau));
+    setTableau(tableau);
+  }
+
 
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>JOJO TODOLIST</IonTitle>
+          <IonTitle className="ion-text-center">JOJO TODOLIST</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <Tab  />
+
+      <IonSegment value="todo" onIonChange={e => filter(e)}>
+        <IonSegmentButton value="todo">
+          <IonLabel>To do</IonLabel>
+        </IonSegmentButton>
+        <IonSegmentButton value="all">
+          <IonLabel>All</IonLabel>
+        </IonSegmentButton>
+      </IonSegment>
+
       <IonContent>
         <IonList>
           {tableau.map((e, index) => {
             return (
-              <MyTask key={index} data={e} index={index}/>
+              <IonItem key={index}>
+                <IonGrid>
+                  <IonRow>
+                    <IonCol>
+                      <MyTask data={e} index={index} />
+                    </IonCol>
+                    <IonCol size="2">
+                      <IonButton
+                        color="light"
+                        onClick={() => { removeTask(index) }}
+                      >
+                        <IonIcon color="danger" icon={trash} />
+                      </IonButton>
+                    </IonCol>
+                  </IonRow>
+                </IonGrid>
+              </IonItem>
             )
           })}
         </IonList>
@@ -73,19 +117,11 @@ const Main: React.FC = () => {
                   }
                 }
               }
-
               onIonChange={e => {
                 setText(e.detail.value!);
               }}>
             </IonInput>
-            <IonButton
-              onClick={
-                e => {
-                  addElement();
-                }
-              }
-            >Add</IonButton>
-
+            <IonButton onClick={() => { addElement(); }}>Add</IonButton>
           </IonRow>
         </IonGrid>
       </IonFooter>
